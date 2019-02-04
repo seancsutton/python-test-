3f7c657f7640c31a91188b9ee30d3d6d55844fd0
Sean Sutton <ssutton21@dxc.com>
2019-02-04 14:11:45 -0600

    changing over to pytest syntax

--- test_2.py
+++ test_2.py
@@ -1,11 +1,8 @@
 # this is a second test file to see if pipline is running
-import unittest
 from test_1 import add
+class TestClass(object):
+    def test_one(self):
+       
+        assert add(1,3) == 4
 
-class First_test(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(1,3), 4)
-    def test_add2(self):
-        self.assertNotEqual(add(2,4), 123)
-    def failing_test(self):
-        self.assertEqual(add(1,4),9)
+
