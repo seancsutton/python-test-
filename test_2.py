6671099025467e1e1c5c6512faa9e12b16aa99b6
Sean Sutton <ssutton21@dxc.com>
2019-02-04 13:05:23 -0600

    trying to get test runner to start up

--- test_2.py
+++ test_2.py
@@ -5,3 +5,6 @@ from test_1 import add
 class First_test(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(1,4), 4)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestStringMethods)
+unittest.TextTestRunner(verbosity=2).run(suite)
