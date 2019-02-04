3edf5e60670f37753184b099b679be13aa4438ac
Sean Sutton <ssutton21@dxc.com>
2019-02-04 13:54:25 -0600

    3rd attempt in getting junit test runner running

--- test_2.py
+++ test_2.py
@@ -4,7 +4,7 @@ from test_1 import add
 
 class First_test(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(1,4), 4)
+        self.assertEqual(add(1,4), 5)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(First_test)
 unittest.TextTestRunner(verbosity=2).run(suite)
